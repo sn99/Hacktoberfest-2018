f5de86d58f79f5fba0897c03cd68eb7605e9ede8
adarshrana205 <adarshrana205@gmail.com>
2018-10-20 09:38:41 +0530

    Added JAVA File

---
+++ hellos/hello_github_adarshrana205.java
@@ -0,0 +1,28 @@
+//LANGUAGE:Java
+
+
+//AUTHOR:Adarsh Rana
+
+//GITHUB:https://github.com/adarshrana205
+
+
+
+
+class hello_github
+
+
+{
+    
+public static void main()
+   
+ 
+{
+        
+
+System.out.println("Hello, Github!");
+  
+ 
+ }
+
+
+}
