e153b94bf4979c438fd49a4837c51a878af6166b
codeX1616 <pratikjoshi1616@gmail.com>
2018-10-28 17:28:46 +0530

    Add Hello, Github! Script

---
+++ hellos/hello_github_codeX1616.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Pratik Joshi
+// GITHUB: https://github.com/codeX1616
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello, Github!"<endl;
+	return 0;
+}
