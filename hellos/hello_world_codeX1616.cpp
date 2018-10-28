5070281b88e93b60602656090d9bafce501332a0
codeX1616 <pratikjoshi1616@gmail.com>
2018-10-28 17:17:23 +0530

    Add Hello, Github! Script

---
+++ hellos/hello_world_codeX1616.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: Rust
+// AUTHOR: Siddharth Naithani
+// GITHUB: https://github.com/sn99
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello, Github"<<endl;
+	return 0;
+}
